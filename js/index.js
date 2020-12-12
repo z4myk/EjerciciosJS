92eb39a8067f9248e2fdcc6789004137b817769c
Sebastian Mosquera <sebimosquera@gmail.com>
2020-12-12 15:30:20 -0300

    se realizo el ejercicio 8

--- js/index.js
+++ js/index.js
@@ -55,3 +55,15 @@ alert("Hola");
 */
 
 
+
+/*                   EJERCICIO 8
+     var n1 = prompt("Escriba un numero");
+    if (n1 % 2 === 0) {
+    document.write("Es divisible por 2");
+    } else {
+    document.write("No es divisible por 2");
+    } 
+*/
+
+
+
