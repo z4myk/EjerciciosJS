e98749b1a64ce255037adbc519fc92e50f3f1557
Sebastian Mosquera <sebimosquera@gmail.com>
2020-12-12 15:24:28 -0300

    se realizo el ejercicio 3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-alert("Hola");
 
-document.write("Hello world");
+/*                   EJERCICIO 1
+alert("Hola");
+*/
+
+
+/*                   EJERCICIO 2
+ document.write("Hello world");
+ */
+
+
+/*                   EJERCICIO 3
+        document.write(3+5);
+*/
+
+
+
